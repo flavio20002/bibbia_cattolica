2e13790fc5a7c3241d9c3efc5959279f3a6d1d33
Flavio <flvb86@gmail.com>
2024-08-05 21:45:23 +0200

    Fix

--- android/app/src/main/kotlin/com/barisi/flavio/bibbiacattolica/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.barisi.flavio.bibbiacattolica
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/bibbia_cattolica/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bibbia_cattolica
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
